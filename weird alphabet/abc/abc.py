16fbaf69d20951a556b2e6927939468456965810
User1 <dwk.03fyongfly_proton@proton.me>
2026-02-12 17:09:50 +0700

    new file

---
+++ weird alphabet/abc/abc.py
@@ -0,0 +1,26 @@
+print("abc")
+import random
+def runpy():
+    user_input= input("codepass 2?")
+
+
+    if user_input == "abc-xyz":
+        print("@a:b*c-x+y+z")
+    elif user_input == "abc+xyz":
+        print("!")
+    else:
+        print("codepass")
+
+
+runpy()
+
+
+
+
+
+
+
+
+
+
+input("\nEnter to exit.")
